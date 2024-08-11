f8f2d38f611a26ca4c6aedcc8e24ae06e3616850
Ashim Maharjan <ciferulquiorra008@gmail.com>
2024-08-11 22:09:35 +1000

    Nav bar design implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  RiFacebookBoxFill,
+  RiInstagramFill,
+  RiLinkedinBoxFill,
+} from "react-icons/ri";
 
 function App() {
+  const links = [
+    {
+      label: "Home",
+      link: "/home",
+    },
+    {
+      label: "Gallery",
+      link: "/home",
+    },
+    {
+      label: "About",
+      link: "/home",
+    },
+    {
+      label: "Shop",
+      link: "/home",
+    },
+    {
+      label: "Sign In",
+      link: "/home",
+    },
+  ];
+
+  const socialLinks = [
+    {
+      site: "facebook",
+      icon: <RiFacebookBoxFill />,
+    },
+    {
+      site: "facebook",
+      icon: <RiInstagramFill />,
+    },
+    {
+      site: "facebook",
+      icon: <RiLinkedinBoxFill />,
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section>
+        <nav className="w-full h-10 bg-gray-800 flex justify-between items-center p-10 text-white">
+          <h2 className="font-serif text-xl">Quuen of Snow Globes</h2>
+
+          <div className="flex items-center gap-8">
+            {links.map((link, index) => (
+              <a key={index} href={link.link}>
+                {link.label}
+              </a>
+            ))}
+          </div>
+
+          <div className="flex items-center gap-2">
+            {socialLinks.map((socialLink, index) => (
+              <a className="text-2xl" key={index} href={socialLink.site}>
+                {socialLink.icon}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </section>
+    </main>
   );
 }
 
